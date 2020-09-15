5d0ddd60aee123efec5e33f1bbe4f7b01ba4f636
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-09-16 00:51:48 +0200

    Can now format the parsed html code into an easy to work with dict.

--- booking_checker.py
+++ booking_checker.py
@@ -1,4 +1,6 @@
 import requests
+import re
+from datetime import date, datetime, time
 from bs4 import BeautifulSoup
 
 
@@ -38,11 +40,38 @@ class BookingChecker:
 
         soup = BeautifulSoup(html, "html.parser")
 
+        # Finding the week and year so it can be used to find the date of each booking.
+        date_text = soup.find("span", class_="bt", text=re.compile("Uge")).contents[0]
+        week = int(date_text[7:9])
+        year = int(date_text[11:-6])
+
         # Iterating through each "day" column in the booking table by searching for the specific html attributes.
         for count, day in enumerate(soup.find_all("td", attrs={"class": "noPad", "width": "14%", "valign": "top",
                                                                "bgcolor": "#E1E1E1", "align": "left"})):
             for booking in day.find_all("td", attrs={"style": "background-color: #F6B448 !important;"}):
-                print(count + 1, booking.attrs["title"])
+                print(self.__format_booking(booking.attrs["title"], count + 1, week, year))
+
+    @staticmethod
+    def __format_booking(title, day, week, year):
+        """
+        Formats the given information into a dictionary with the keys [name, start datetime, end datetime].
+
+        :param title: The html title of the table element showing a single booking.
+        :param day: The week day number as an integer.
+        :param week: The week number as an integer.
+        :param year: The year as an integer.
+        :return: A dictionary containing the extracted information about the booking.
+        """
+        name = title[5:-22]
+        start_time = title[-12:-7]
+        end_time = title[-6:-1]
+
+        start_date_time = datetime.combine(date.fromisocalendar(year=year, week=week, day=day),
+                                           time(hour=int(start_time[:2]), minute=int(start_time[-2:])))
+        end_date_time = datetime.combine(date.fromisocalendar(year=year, week=week, day=day),
+                                         time(hour=int(end_time[:2]), minute=int(end_time[-2:])))
+
+        return {"name": name, "start datetime": start_date_time, "end datetime": end_date_time}
 
 
 test = BookingChecker("test")
