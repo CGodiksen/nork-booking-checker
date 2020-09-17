dd335c31f95db4912c760f4500433d0af89bfb10
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-09-17 21:38:32 +0200

    Now runs through main. Scheduler runs the checker every 15 minutes.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from bs4 import BeautifulSoup
+import schedule
+import time
+from booking_checker import BookingChecker
 
 
+if __name__ == '__main__':
+    booking_checker = BookingChecker("Christian Godiksen")
+
+    schedule.every(15).minutes.do(booking_checker.booking_checker_job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
