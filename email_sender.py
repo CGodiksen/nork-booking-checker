6ba4aab8dcebe6a872baa3aeb8146e9c8c0cfc49
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-09-17 12:15:54 +0200

    Added method for sending messages.

--- email_sender.py
+++ email_sender.py
@@ -56,5 +56,20 @@ class EmailSender:
 
         return {'raw': base64.urlsafe_b64encode(message.as_string())}
 
+    def send_message(self, user_id, message):
+        """Send an email message.
+
+        :param user_id: User's email address. The special value "me" can be used to indicate the authenticated user.
+        :param message: Message to be sent.
+        :return: Sent Message.
+        """
+        try:
+            message = (self. service.users().messages().send(userId=user_id, body=message)
+                       .execute())
+            print('Message Id: %s' % message['id'])
+            return message
+        except errors.HttpError as error:
+            print('An error occurred: %s' % error)
+
 
 test = EmailSender()
